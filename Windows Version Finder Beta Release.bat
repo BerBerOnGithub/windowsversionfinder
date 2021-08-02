a6e8828b216ab9bcadb91fef490921f32add50b2
BerBerOnGithub <86917016+BerBerOnGithub@users.noreply.github.com>
2021-08-02 18:00:17 +0200

    Add files via upload

---
+++ Windows Version Finder Beta Release.bat
@@ -0,0 +1,456 @@
+@ECHO OFF
+mode con:cols=120 lines=30
+@echo Windows Version Finder Public Beta 0.1
+@echo
+@echo Today is %date% %time%.
+:choice1
+set /P c=Do you want to know your Windows version? [Y/N]
+if /I "%c%" EQU "Y" goto :choice2
+if /I "%c%" EQU "N" goto :somewhere_else
+goto :choice1
+:choice2
+@echo Warning! This will not work with Android.
+set /P c=Are you sure you want to run the script? [Y/N]
+if /I "%c%" EQU "Y" goto :somewhere
+if /I "%c%" EQU "N" goto :somewhere_else
+goto :choice2
+:somewhere
+@echo Searching For Windows Version
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 1 out of 4
+@echo Retrieving Windows version. Progress: 0%%
+@echo Total Progress: 0%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.                                                                                               
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 1 out of 4
+@echo Retrieving Windows version.. Progress: 14%%
+@echo Total Progress: 3%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.                                                                                               
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 1 out of 4
+@echo Retrieving Windows version... Progress: 32%%
+@echo Total Progress: 6%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.                                                                                               
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 1 out of 4
+@echo Retrieving Windows version. Progress: 66%%
+@echo Total Progress: 10%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.                                                                                               
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 1 out of 4
+@echo Retrieving Windows version.. Progress: 83%%
+@echo Total Progress: 13%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.                                                                                               
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 1 out of 4
+@echo Retrieving Windows version... Progress: 94%%
+@echo Total Progress: 17%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!                                                                                  
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 1 out of 4
+@echo Retrieving Successful! Progress: 100%%
+@echo Total Progress: 23%%
+ping -n 4 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!                                                                                   
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 2 out of 4
+@echo Retrieving Windows build version. Progress: 0%%
+@echo Total Progress: 23%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!                                                                                  
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 2 out of 4
+@echo Retrieving Windows build version.. Progress: 18%%
+@echo Total Progress: 27%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!                                                                                  
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 2 out of 4
+@echo Retrieving Windows build version... Progress: 36%%
+@echo Total Progress: 34%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!                                                                                  
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 2 out of 4
+@echo Retrieving Windows build version. Progress: 42%%
+@echo Total Progress: 39%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!                                                                                  
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 2 out of 4
+@echo Retrieving Windows build version.. Progress: 73%%
+@echo Total Progress: 47%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!                                                                                  
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 2 out of 4
+@echo Retrieving Windows build version... Progress: 84%%
+@echo Total Progress: 52%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!                                                                
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 2 out of 4
+@echo Windows Build Version Found! Progress:  100%%
+@echo Total Progress: 54%%
+ping -n 4 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!                                                                
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 3 out of 4
+@echo Retrieving Windows edition. Progress:  0%%
+@echo Total Progress: 54%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!                                                                   
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 3 out of 4
+@echo Retrieving Windows edition.. Progress:  12%%
+@echo Total Progress: 56%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!                                                                   
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 3 out of 4
+@echo Retrieving Windows edition... Progress:  30%%
+@echo Total Progress: 57%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!                                                                
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 3 out of 4
+@echo Retrieving Windows edition. Progress:  42%%
+@echo Total Progress: 59%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!                                                                 
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 3 out of 4
+@echo Retrieving Windows edition.. Progress:  69%%
+@echo Total Progress: 62%%
+ping -n 2 127.0.0.1 > nul
+cls                                                        
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!                                                                    
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 3 out of 4
+@echo Retrieving Windows edition... Progress:  77%%
+@echo Total Progress: 67%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!              Successful!                                          
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Step 3 out of 4
+@echo Windows Edition Search Successful! Progress:  100%%
+@echo Total Progress: 77%%
+ping -n 4 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  Searching For Version In The Database
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!              Successful!                                                             
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Final Step
+@echo Searching for matching version in the database. Progress:  0%%
+@echo Total Progress: 80%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  Searching For Version In The Database
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!              Successful!                                                
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Final Step
+@echo Searching for matching version in the database.. Progress:  14%%
+@echo Total Progress: 83%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  Searching For Version In The Database
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!              Successful!                                                
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Final Step
+@echo Searching for matching version in the database... Progress:  28%%
+@echo Total Progress: 86%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  Searching For Version In The Database
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!              Successful!                                               
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Final Step
+@echo Searching for matching version in the database. Progress:  57%%
+@echo Total Progress: 93%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  Searching For Version In The Database
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!              Successful!                                                 
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Final Step
+@echo Searching for matching version in the database.. Progress:  78%%
+@echo Total Progress: 96%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  Searching For Version In The Database
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!              Successful!                                                
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Final Step
+@echo Searching for matching version in the database... Progress:  90%%
+@echo Total Progress: 98%%
+ping -n 2 127.0.0.1 > nul
+cls
+@echo Searching For Windows Version  Searching For Build  Searching For Windows Edition  Searching For Version In The Database
+@echo ------------------------------------------------------------------------------------------------------------------------
+@echo.         Successful!               Successful!              Successful!                                Successful!                
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Final Step
+@echo Search Successful! Progress:  100%%
+@echo Total Progress: 100%%
+ping -n 4 127.0.0.1 > nul
+cls
+@echo off
+ver | find "5.1" > nul
+ 
+if errorlevel = 1 goto next0
+if errorlevel = 0 goto xp
+ 
+:next0
+ver | find "6.0" > nul
+if errorlevel = 1 goto next
+if errorlevel = 0 goto win vista
+ 
+:next
+ver | find "6.1"
+if errorlevel = 1 goto next1
+if errorlevel = 0 goto win7
+ 
+:next1
+ver | find "6.2" > nul
+if errorlevel = 1 goto next2
+if errorlevel = 0 goto win8
+ 
+:next2
+ver | find "6.3" > nul
+if errorlevel = 1 goto next3
+if errorlevel = 0 goto win8.1
+ 
+:next3
+ver | find "6.3" > nul
+if errorlevel = 1 goto next4
+if errorlevel = 0 goto win8.1
+ 
+:next4
+ver | find "10.0" > nul
+if errorlevel = 1 goto other
+if errorlevel = 0 goto win10
+ 
+:xp
+echo Your version is Windows XP.
+timeout 3 /nobreak >nul
+goto :exitprompt
+ 
+:win vista
+echo Your version is Windows Vista.
+timeout 3 /nobreak >nul
+goto :exitprompt
+  
+:win7
+echo Your version is Windows 7.
+timeout 3 /nobreak >nul
+goto :exitprompt
+ 
+:win8
+echo Your version is Windows 8.
+timeout 3 /nobreak >nul
+goto :exitprompt
+ 
+:win8.1
+echo Your version is Windows 8.1.
+timeout 3 /nobreak >nul
+goto :exitprompt
+ 
+:win10
+echo Your version is Windows 10.
+timeout 3 /nobreak >nul
+goto :exitprompt
+ 
+:other
+winver
+goto :exitprompt
+
+:exitprompt
+set /P c=Would you like to exit? [Y/N]
+if /I "%c%" EQU "Y" goto :exit
+if /I "%c%" EQU "N" goto :igiveup
+:exit
+@echo Don't be sad because it's gone, be happy cause it happened.
+ping -n 4 127.0.0.1 > nul
+exit
+
+:somewhere_else
+
+@echo Okay, if you say so.
+pause
+exit
+
+:igiveup
+set /P c=Would you like to go on a website?
+if /I "%c%" EQU "Y" goto :turd
+if /I "%c%" EQU "N" goto :somewhere_else
+
+:turd
+set /P c=Type in your website. Use this mode: www.example.com. Currently only social media sites work.
+if /I "%c%" EQU "www.google.com" start "" http://www.google.com
+if /I "%c%" EQU "www.facebook.com" start "" http://www.facebook.com
+if /I "%c%" EQU "www.twitter.com" start "" http://www.twitter.com
+if /I "%c%" EQU "www.instagram.com" start "" http://www.instagram.com
+if /I "%c%" EQU "www.reddit.com" start "" http://www.reddit.com
+if /I "%c%" EQU "www.youtube.com" start "" http://www.youtube.com
+if /I "%c%" EQU "www.twitch.com" start "" http://www.twitch.com
+if /I "%c%" EQU "www.vk.com" start "" http://www.vk.com
+if /I "%c%" EQU "www.discord.com" start "" http://www.discord.com
+exit
+  
